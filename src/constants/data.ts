dfd28b57409b65ea4c06a7ede4f449f20a4c26f7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-02 18:25:48 +0700

    initial commit

---
+++ src/constants/data.ts
@@ -0,0 +1,137 @@
+import { NavItem } from '@/types';
+
+export type Product = {
+  photo_url: string;
+  name: string;
+  description: string;
+  created_at: string;
+  price: number;
+  id: number;
+  category: string;
+  updated_at: string;
+};
+
+//Info: The following data is used for the sidebar navigation and Cmd K bar.
+export const navItems: NavItem[] = [
+  {
+    title: 'Dashboard',
+    url: '/dashboard/overview',
+    icon: 'dashboard',
+    isActive: false,
+    shortcut: ['d', 'd'],
+    items: [] // Empty array as there are no child items for Dashboard
+  },
+  {
+    title: 'Tracker',
+    url: '/dashboard/tracker',
+    icon: 'tracker',
+    shortcut: ['t', 't'],
+    isActive: false,
+    items: [] // No child items
+  },
+  {
+    title: 'End Device',
+    url: '/dashboard/end-device',
+    icon: 'devices',
+    shortcut: ['e', 'e'],
+    isActive: false,
+    items: [] // No child items
+  },
+  {
+    title: 'Base Station',
+    url: '/dashboard/base-station',
+    icon: 'baseStation',
+    shortcut: ['bs', 'bs'],
+    isActive: false,
+    items: [] // No child items
+  },
+  {
+    title: 'Maps',
+    url: '/dashboard/maps',
+    icon: 'maps',
+    shortcut: ['m', 'm'],
+    isActive: false,
+    items: [] // No child items
+  },
+  {
+    title: 'Account',
+    url: '#', // Placeholder as there is no direct link for the parent
+    icon: 'billing',
+    isActive: true,
+
+    items: [
+      {
+        title: 'Profile',
+        url: '/dashboard/profile',
+        icon: 'userPen',
+        shortcut: ['m', 'm']
+      },
+      {
+        title: 'Login',
+        shortcut: ['l', 'l'],
+        url: '/',
+        icon: 'login'
+      }
+    ]
+  },
+  {
+    title: 'Kanban',
+    url: '/dashboard/kanban',
+    icon: 'kanban',
+    shortcut: ['k', 'k'],
+    isActive: false,
+    items: [] // No child items
+  }
+];
+
+export interface SaleUser {
+  id: number;
+  name: string;
+  email: string;
+  amount: string;
+  image: string;
+  initials: string;
+}
+
+export const recentSalesData: SaleUser[] = [
+  {
+    id: 1,
+    name: 'Olivia Martin',
+    email: 'olivia.martin@email.com',
+    amount: '+$1,999.00',
+    image: 'https://api.slingacademy.com/public/sample-users/1.png',
+    initials: 'OM'
+  },
+  {
+    id: 2,
+    name: 'Jackson Lee',
+    email: 'jackson.lee@email.com',
+    amount: '+$39.00',
+    image: 'https://api.slingacademy.com/public/sample-users/2.png',
+    initials: 'JL'
+  },
+  {
+    id: 3,
+    name: 'Isabella Nguyen',
+    email: 'isabella.nguyen@email.com',
+    amount: '+$299.00',
+    image: 'https://api.slingacademy.com/public/sample-users/3.png',
+    initials: 'IN'
+  },
+  {
+    id: 4,
+    name: 'William Kim',
+    email: 'will@email.com',
+    amount: '+$99.00',
+    image: 'https://api.slingacademy.com/public/sample-users/4.png',
+    initials: 'WK'
+  },
+  {
+    id: 5,
+    name: 'Sofia Davis',
+    email: 'sofia.davis@email.com',
+    amount: '+$39.00',
+    image: 'https://api.slingacademy.com/public/sample-users/5.png',
+    initials: 'SD'
+  }
+];
