fa32885ae5fb81b9be2dc06e9293659c3a2f509b
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-03 23:17:57 +0700

    feat<>: create api dynamic params

---
+++ src/app/api/devices/[id]/route.ts
@@ -0,0 +1,114 @@
+import { supabase } from "@/lib/supabase/server";
+import { Delete } from "lucide-react";
+import { NextResponse } from "next/server";
+
+type RouteParams = {
+   params: {
+      id: string
+   };
+}
+
+export async function GET(req: Request, { params }: RouteParams, ) {
+   try {
+      const { id } = params;
+
+      const { data, error } =  await supabase
+         .from('devices')
+         .select('*')
+         .eq('id', id)
+         .single();
+
+      if(error) {
+         return NextResponse.json({
+            error: 'Device not found',
+         }, { status: 404 });
+      }
+
+      return NextResponse.json({
+         success: true,
+         data,
+      }, { status: 200 });
+
+   } catch (error) {
+      if(error instanceof Error) {
+         return NextResponse.json({
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+   }
+}
+
+export async function PUT(req: Request, { params }: RouteParams) {
+   try {
+      const { id } = params;
+      const body = await req.json();
+      const { name, description } = body;
+
+      if(!name || !description) {
+         return NextResponse.json({
+            error: 'Name and description are required'
+         }, { status: 400 });
+      }
+
+      const { data, error } = await supabase
+         .from('devices')
+         .update({ name, description })
+         .eq('id', id)
+         .select()
+         .single();
+
+
+      if(error) {
+         return NextResponse.json({
+            error: 'Failed to update data',
+            message: error.message
+         }, { status: 500 });
+      }
+
+      return NextResponse.json({
+         success: true,
+         data
+      }, { status: 200 });
+
+   } catch(error) {
+      if(error instanceof Error) {
+         return NextResponse.json({
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+   }
+}
+
+export async function DELETE(req: Request, { params }: RouteParams) {
+   try {
+      const { id } = params;
+      const {data, error} = await supabase
+         .from('devices')
+         .delete()
+         .eq('id', id)
+         .select()
+      
+      if(error) {
+         return NextResponse.json({
+            error: 'Failed to delete data',
+            message: error.message
+         }, { status: 500 });
+      }
+
+      return NextResponse.json({
+         success: true,
+         message: 'Device deleted successfully',
+         data,
+      }, { status: 200 });
+   
+   } catch(error) {
+      if(error instanceof Error) {
+         return NextResponse.json({
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+   }
+}
