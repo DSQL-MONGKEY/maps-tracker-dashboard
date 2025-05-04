5220d60a1e9f8e3e66bc5e68d4d2510de36f6e7c
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-04 14:19:41 +0700

    feat<>: change folder name, create POST and GET all request handler

---
+++ src/app/api/trackings/route.ts
@@ -0,0 +1,86 @@
+import { NextResponse } from 'next/server'
+import { supabase } from '@/lib/supabase/server'
+
+
+export async function POST(req: Request) {
+   try {
+      const body = await req.json();
+
+      const { 
+         device_id,
+         latitude,
+         longitude,
+         altitude,
+         rssi,
+         message,
+         snr,
+         speed,
+         is_emergency
+      } = body;
+
+      const { data, error } = await supabase
+         .from('trackings')
+         .insert({ 
+            device_id, latitude, longitude, altitude, rssi, message, snr, speed, is_emergency 
+         })
+         .select();
+
+      if(error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500});
+      }
+
+      return NextResponse.json({
+         success: true,
+         data,
+      }, { status: 201});
+
+   } catch(error) {
+
+      if(error instanceof Error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500});
+      }
+
+   }
+}
+
+export async function GET() {
+   try {
+
+      const { data, error }= await supabase
+         .from('trackings')
+         .select('*')
+         .order('created_at', { ascending: false });
+
+      if(error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500});
+      }
+
+      return NextResponse.json({
+         success: true,
+         data,
+      }, { status: 200});
+
+   } catch(error) {
+
+      if(error instanceof Error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message,
+         }, { status: 500});
+      }
+
+   }
+}
