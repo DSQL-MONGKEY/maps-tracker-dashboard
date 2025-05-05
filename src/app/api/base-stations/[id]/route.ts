901c2d8baaa1f8cd4295879d62723970ad337b42
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-05 20:15:11 +0700

    feat<>: create dynamic route params api

---
+++ src/app/api/base-stations/[id]/route.ts
@@ -0,0 +1,130 @@
+import { supabase } from "@/lib/supabase/server";
+import { RouteParams } from "@/types";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params }:RouteParams) {
+   try {
+      const { id } = params;
+         
+         const { data, error } = await supabase
+            .from('base_stations')
+            .select('*')
+            .eq('id', id)
+            .single();
+   
+         if(error) {
+            return NextResponse.json({
+               success: false,
+               error: 'Base station not found',
+            }, { status: 404 });
+         }
+   
+         return NextResponse.json({
+            success: true,
+            data,
+         }, { status: 200 });
+
+   } catch(error) {
+   
+      if(error instanceof Error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+
+   }
+}
+
+export async function PUT(req: Request, { params }:RouteParams) {
+   try {
+      const { id } = params;
+      const body = await req.json();
+      const {
+         name,
+         location_name,
+         latitude,
+         longitude,
+         description,
+         status,
+      } = body;
+
+
+      const { data, error } = await supabase
+         .from('base_stations')
+         .update({
+            name,
+            location_name,
+            latitude,
+            longitude,
+            description,
+            status,
+         })
+         .eq('id', id)
+         .select()
+         .single();
+
+      if(error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+
+      return NextResponse.json({
+         success: true,
+         data,
+      }, { status: 200 });
+   
+   } catch(error) {
+
+         if(error instanceof Error) {
+            return NextResponse.json({
+               success: false,
+               error: 'An error occurred while processing your request',
+               details: error.message
+            }, { status: 500 });
+         }
+
+   }
+}
+
+export async function DELETE(req: Request, { params }:RouteParams) {
+   try {
+      const { id } = params;
+
+      const { data, error } = await supabase
+         .from('base_stations')
+         .delete()
+         .eq('id', id)
+         .select()
+         .single();
+
+      if(error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+
+      return NextResponse.json({
+         success: true,
+         data,
+      }, { status: 200 });
+
+   } catch(error) {
+
+      if(error instanceof Error) {
+         return NextResponse.json({
+            success: false,
+            error: 'An error occurred while processing your request',
+            details: error.message
+         }, { status: 500 });
+      }
+
+   }
+}
+
