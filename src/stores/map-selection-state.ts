ebb9253cf2f75717da7aad3c10d491d44cad7dbd
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-08 15:03:44 +0700

    feat<>: add latestMode state for disabled conditional rendering

--- src/stores/map-selection-state.ts
+++ src/stores/map-selection-state.ts
@@ -1,20 +1,23 @@
-import { set } from 'date-fns';
 import { create } from 'zustand'
 
 
 interface MapSelecttionState {
    fromId: string | null;
    toId: string | null;
+   latestMode: boolean;
    setFromId: (id: string | null) => void;
    setToId: (id: string | null) => void;
+   setLatestMode: (latestMode: boolean) => void;
    clearSelection: () => void;
 }
 
 export const useMapSelectionStore = create<MapSelecttionState>((set) => ({
    fromId: null,
    toId: null,
+   latestMode: false,
    setFromId: (id) => set({ fromId: id }),
    setToId: (id) => set({ toId: id }),
-   clearSelection: () => set({ fromId: null, toId: null }),   
+   setLatestMode: () => set(prev => ({ latestMode: !prev.latestMode })),
+   clearSelection: () => set({ fromId: '', toId: '' }),   
 }));
 
