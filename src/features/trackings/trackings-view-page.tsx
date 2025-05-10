380e23ad4df0ac36199df45f5d5d79d753d1d4c2
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-10 22:58:24 +0700

    feat<>: create view page tracking component

---
+++ src/features/trackings/trackings-view-page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from 'next/navigation';
+import TrackingsForm from './trackings-form';
+import { getTrackingById } from './api/getTrackingById';
+
+type TTrackingViewPage = {
+  trackingId: string;
+};
+
+export default async function TrackingViewPage({
+  trackingId
+}: TTrackingViewPage) {
+  let tracking = null;
+  let pageTitle = 'Create New Tracking Record';
+
+  if (trackingId !== 'new') {
+    const response = await getTrackingById(String(trackingId));
+    
+    const { data } = response ? await response.json() : { data: null };
+
+    tracking = data;
+
+    if (!data) {
+      notFound();
+    }
+    pageTitle = `Edit Tracking Record`;
+  }
+
+  return <TrackingsForm initialData={tracking} pageTitle={pageTitle} />;
+}
