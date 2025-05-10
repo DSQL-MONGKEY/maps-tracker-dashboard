7d670e44f010b63ce586017f91ba36aa3690f178
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-10 22:56:17 +0700

    feat<>: change props into tracking data value and adjust types for viewPage

--- src/app/dashboard/trackings/[trackingId]/page.tsx
+++ src/app/dashboard/trackings/[trackingId]/page.tsx
@@ -1,13 +1,13 @@
 import FormCardSkeleton from '@/components/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
 import { Suspense } from 'react';
-import ProductViewPage from '@/features/products/components/product-view-page';
+import TrackingViewPage from '@/features/trackings/trackings-view-page';
 
 export const metadata = {
-  title: 'Dashboard : Product View'
+  title: 'Dashboard : Tracking View'
 };
 
-type PageProps = { params: Promise<{ productId: string }> };
+type PageProps = { params: Promise<{ trackingId: string }> };
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
@@ -15,7 +15,7 @@ export default async function Page(props: PageProps) {
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
         <Suspense fallback={<FormCardSkeleton />}>
-          <ProductViewPage productId={params.productId} />
+          <TrackingViewPage trackingId={params.trackingId} />
         </Suspense>
       </div>
     </PageContainer>
