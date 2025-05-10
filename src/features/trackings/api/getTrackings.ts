427741d0fa12574eff6d8617bb2c35d2803e17b7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-10 22:59:05 +0700

    feat<>: create api handler for get tracking data

---
+++ src/features/trackings/api/getTrackings.ts
@@ -0,0 +1,37 @@
+import { supabase } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+
+export async function getTrackings() {
+   try {
+   
+         const { data, error }= await supabase
+            .from('trackings')
+            .select(`*, devices(name, type)`)
+            .order('created_at', { ascending: false });
+   
+         if(error) {
+            return NextResponse.json({
+               success: false,
+               error: 'An error occurred while processing your request',
+               details: error.message
+            }, { status: 500});
+         }
+   
+         return NextResponse.json({
+            success: true,
+            data,
+         }, { status: 200});
+   
+      } catch(error) {
+   
+         if(error instanceof Error) {
+            return NextResponse.json({
+               success: false,
+               error: 'An error occurred while processing your request',
+               details: error.message,
+            }, { status: 500});
+         }
+   
+      }
+}
