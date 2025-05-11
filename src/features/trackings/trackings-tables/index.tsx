1e47d19362668ad3f841afcdd28aa56482d78f20
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-11 22:51:44 +0700

    feat<>: change function types name with TrackingDataTable

--- src/features/trackings/trackings-tables/index.tsx
+++ src/features/trackings/trackings-tables/index.tsx
@@ -8,16 +8,16 @@ import { useDataTable } from '@/hooks/use-data-table';
 import { ColumnDef } from '@tanstack/react-table';
 import { parseAsInteger, useQueryState } from 'nuqs';
 
-interface ProductTableParams<TData, TValue> {
+interface TrackingTableParams<TData, TValue> {
   data: TData[];
   totalItems: number;
   columns: ColumnDef<TData, TValue>[];
 }
-export function ProductTable<TData, TValue>({
+export function TrackingDataTable<TData, TValue>({
   data,
   totalItems,
   columns
-}: ProductTableParams<TData, TValue>) {
+}: TrackingTableParams<TData, TValue>) {
   const [pageSize] = useQueryState('perPage', parseAsInteger.withDefault(10));
 
   const pageCount = Math.ceil(totalItems / pageSize);
