d8d03b80d266204faad21f2b06e3856b0caeda52
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-13 15:49:43 +0700

    feat<>: add basic structure request post

--- src/features/trackings/api/add-tracking.ts
+++ src/features/trackings/api/add-tracking.ts
@@ -1,3 +1,26 @@
-export async function addTracking(deviceName, holderName, latitude, longitude, emergencyStatus) {
-   
+
+type TTracking = {
+   deviceId: string;
+   rssi?: string;
+   snr?: string;
+   latitude: string;
+   longitude: string;
+   emergencyStatus: boolean;
+}
+
+export async function addTracking({deviceId, rssi, snr, latitude, longitude, emergencyStatus}:TTracking) {
+   const response = await fetch('/api/trackings', {
+      method: 'POST',
+      body: JSON.stringify({
+         device_id: deviceId,
+         rssi: rssi,
+         snr: snr,
+         latitude: latitude,
+         longitude: longitude,
+         is_emergency: emergencyStatus
+      }),
+   });
+   const data = await response.json();
+
+   return data;
 }
