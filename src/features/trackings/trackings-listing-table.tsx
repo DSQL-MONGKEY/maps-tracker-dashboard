ff3805c39b84a33d4af8c5f7ad1403905c4182e0
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-16 23:45:26 +0700

    feat<>: create listing table wrapper client component for swr fetch optimistic

---
+++ src/features/trackings/trackings-listing-table.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import useSWR from "swr";
+import { TrackingDataTable } from "./trackings-tables";
+import { columns } from "./trackings-tables/columns";
+import { fetcher } from "@/lib/fetcher";
+import { toast } from "sonner";
+import { DataTableSkeleton } from "@/components/ui/table/data-table-skeleton";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function TrackingsListingTable({ filter }: any) {
+   const { data, error, isLoading } = useSWR('/api/trackings', fetcher);
+
+   if(isLoading) return (
+      <DataTableSkeleton columnCount={5} rowCount={8} filterCount={2} />
+   )
+
+   const trackingData = data.data ?? [];
+   const totalItems = data.data.length ?? 0;
+
+
+   if(error) {
+      toast('Error fetching data', {
+         description: `details: ${error}`
+      })
+   }
+
+   return (
+      <TrackingDataTable
+         data={trackingData}
+         totalItems={totalItems}
+         columns={columns}
+      />
+   );
+}
