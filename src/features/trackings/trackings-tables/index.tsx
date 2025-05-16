01b95a17aab7bbc0c1d9bf8c74fd8f08c0229ab7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-16 23:46:42 +0700

    chore<>: change commentar

--- src/features/trackings/trackings-tables/index.tsx
+++ src/features/trackings/trackings-tables/index.tsx
@@ -23,8 +23,8 @@ export function TrackingDataTable<TData, TValue>({
   const pageCount = Math.ceil(totalItems / pageSize);
 
   const { table } = useDataTable({
-    data, // product data
-    columns, // product columns
+    data, // trackings data
+    columns, // trackings columns
     pageCount: pageCount,
     shallow: false, //Setting to false triggers a network request with the updated querystring.
     debounceMs: 500
