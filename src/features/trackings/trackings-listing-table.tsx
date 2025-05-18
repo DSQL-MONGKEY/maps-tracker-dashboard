cf25934e4acb6dd70d71f0f5e868178820db413d
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-18 14:44:00 +0700

    feat<>: add name alias for destructure data property from SWR to "response"

--- src/features/trackings/trackings-listing-table.tsx
+++ src/features/trackings/trackings-listing-table.tsx
@@ -9,16 +9,16 @@ import { DataTableSkeleton } from "@/components/ui/table/data-table-skeleton";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function TrackingsListingTable({ filter }: any) {
-   const { data, error, isLoading } = useSWR('/api/trackings', fetcher);
+   const { data:response, error, isLoading } = useSWR('/api/trackings', fetcher);
 
    if(isLoading) return (
       <DataTableSkeleton columnCount={5} rowCount={8} filterCount={2} />
    )
 
-   const trackingData = data.data ?? [];
-   const totalItems = data.data.length ?? 0;
+   const trackingData = response.data ?? [];
+   const totalItems = response.data.length ?? 0;
 
-
+   
    if(error) {
       toast('Error fetching data', {
          description: `details: ${error}`
