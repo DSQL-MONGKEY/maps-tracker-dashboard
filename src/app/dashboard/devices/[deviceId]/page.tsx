86b2c80f8a0abeaaae8b336e2de991a8ca897160
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-30 23:30:59 +0700

    feat<>: change name to device component

--- src/app/dashboard/devices/[deviceId]/page.tsx
+++ src/app/dashboard/devices/[deviceId]/page.tsx
@@ -1,13 +1,13 @@
 import FormCardSkeleton from '@/components/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
 import { Suspense } from 'react';
-import TrackingViewPage from '@/features/trackings/trackings-view-page';
+import DeviceViewPage from '@/features/devices/components/devices-view-page';
 
 export const metadata = {
   title: 'Dashboard : Device View'
 };
 
-type PageProps = { params: Promise<{ trackingId: string }> };
+type PageProps = { params: Promise<{ deviceId: string }> };
 
 export default async function Page(props: PageProps) {
   const params = await props.params;
@@ -15,7 +15,7 @@ export default async function Page(props: PageProps) {
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
         <Suspense fallback={<FormCardSkeleton />}>
-          <TrackingViewPage trackingId={params.trackingId} />
+          <DeviceViewPage deviceId={params.deviceId} />
         </Suspense>
       </div>
     </PageContainer>
