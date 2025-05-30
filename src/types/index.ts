cc457c31a536da9279013db2fe2dae5f2d4e767e
Dimpfe <dimasprasetyo7696@gmail.com>
2025-05-30 23:36:24 +0700

    feat<>: add register and climberUser types

--- src/types/index.ts
+++ src/types/index.ts
@@ -51,20 +51,38 @@ export interface Tracking {
   created_at: string;
   updated_at: string;
   devices: {
-      name: string,
-      type: string,
+    name: string,
+    type: string,
   }
   climber_users: {
     name: string
   }
 } 
 
-
 export interface Devices {
   created_at: string;
+  device_code: string;
   description: string;
   id: string;
   name: string;
   status: boolean;
   type: string;
+}
+
+export interface RegisterDevices {
+  id: number;
+  climber_user_id: string;
+  device_id: string;
+  registered_at: string;
+  unregistered_at: string | null;
+  is_active: boolean;
+}
+
+export interface ClimberUser {
+  id: string;
+  name: string;
+  phone: string;
+  email: string;
+  address: string;
+  created_at: string;
 }
