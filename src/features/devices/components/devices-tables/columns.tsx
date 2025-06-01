bd2c0babd82b65049dc42bb8f2af26d9f9f3a10f
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-01 13:56:45 +0700

    feat<>: add device code column

--- src/features/devices/components/devices-tables/columns.tsx
+++ src/features/devices/components/devices-tables/columns.tsx
@@ -10,6 +10,15 @@ import { formatDate } from '@/lib/format';
 
 export const columns: ColumnDef<Devices>[] = [
   {
+    id: 'deviceCode',
+    accessorKey: 'device_code',
+    header: ({ column }: { column: Column<Devices, unknown> }) => (
+      <DataTableColumnHeader column={column} title='Device Code' />
+    ),
+    cell: ({ cell }) => <div>{cell.getValue<Devices['device_code']>()}</div>,
+    enableColumnFilter: true
+  },
+  {
     id: 'name',
     accessorKey: 'name',
     header: ({ column }: { column: Column<Devices, unknown> }) => (
@@ -61,6 +70,22 @@ export const columns: ColumnDef<Devices>[] = [
     header: 'Created At',
     cell: ({ cell }) => {
       const formattedDate = formatDate(cell.getValue<Devices['created_at']>(), {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: false
+      });
+
+      return (
+        <span>{formattedDate}</span>
+      )
+    }
+  },
+  {
+    accessorKey: 'updated_at',
+    header: 'Updated At',
+    cell: ({ cell }) => {
+      const formattedDate = formatDate(cell.getValue<Devices['updated_at']>(), {
         hour: 'numeric',
         minute: 'numeric',
         second: 'numeric',
