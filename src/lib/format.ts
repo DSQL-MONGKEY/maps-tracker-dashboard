4a6079229f7ea0b0d8c7ffb3b6504b7b049eaf03
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-03 19:03:38 +0700

    feat<>: add specific timezone in Asia/jakarta

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,11 +1,13 @@
 export function formatDate(
   date: Date | string | number | undefined,
-  opts: Intl.DateTimeFormatOptions = {}
+  opts: Intl.DateTimeFormatOptions = {},
+  timeZone: string = 'Asia/jakarta'
 ) {
   if (!date) return '';
 
   try {
     return new Intl.DateTimeFormat('en-US', {
+      timeZone,
       month: opts.month ?? 'long',
       day: opts.day ?? 'numeric',
       year: opts.year ?? 'numeric',
