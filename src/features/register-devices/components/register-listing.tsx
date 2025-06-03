e66d5cd974bc026f92dfee346e33dac1913262e5
Dimpfe <dimasprasetyo7696@gmail.com>
2025-06-03 19:09:03 +0700

    feat<>: craete register listing for register-device

---
+++ src/features/register-devices/components/register-listing.tsx
@@ -0,0 +1,24 @@
+import { searchParamsCache } from '@/lib/searchparams';
+import RegisterListingTable from './register-listing-table';
+
+type RegisterListingPage = {};
+
+export default async function RegisterListingPage({}: RegisterListingPage) {
+  // Showcasing the use of search params cache in nested RSCs
+  const page = searchParamsCache.get('page');
+  const search = searchParamsCache.get('name');
+  const pageLimit = searchParamsCache.get('perPage');
+  const categories = searchParamsCache.get('category');
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const filters = {
+    page,
+    limit: pageLimit,
+    ...(search && { search }),
+    ...(categories && { categories: categories })
+  };
+
+  return (
+    <RegisterListingTable />
+  );
+}
