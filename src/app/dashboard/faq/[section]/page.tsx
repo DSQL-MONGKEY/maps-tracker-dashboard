619394843d4c2981caa6c631f16267b330454bc0
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-04 16:53:56 +0700

    fix<>: bug on props

--- src/app/dashboard/faq/[section]/page.tsx
+++ src/app/dashboard/faq/[section]/page.tsx
@@ -1,7 +1,6 @@
 import FormCardSkeleton from '@/components/form-card-skeleton';
 import PageContainer from '@/components/layout/page-container';
 import { Suspense } from 'react';
-import DeviceViewPage from '@/features/devices/components/devices-view-page';
 
 export const metadata = {
   title: 'Dashboard : FAQ'
@@ -10,12 +9,10 @@ export const metadata = {
 type PageProps = { params: Promise<{ section: string }> };
 
 export default async function Page(props: PageProps) {
-  const params = await props.params;
   return (
     <PageContainer scrollable>
       <div className='flex-1 space-y-4'>
         <Suspense fallback={<FormCardSkeleton />}>
-          <DeviceViewPage deviceId={params.deviceId} />
         </Suspense>
       </div>
     </PageContainer>
