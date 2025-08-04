24dd91b7b157e612a2eefc1184f43df649cc6f4f
Dimpfe <dimasprasetyo7696@gmail.com>
2025-08-04 16:47:53 +0700

    feat<>: initial create

---
+++ src/app/dashboard/faq/[section]/page.tsx
@@ -0,0 +1,23 @@
+import FormCardSkeleton from '@/components/form-card-skeleton';
+import PageContainer from '@/components/layout/page-container';
+import { Suspense } from 'react';
+import DeviceViewPage from '@/features/devices/components/devices-view-page';
+
+export const metadata = {
+  title: 'Dashboard : FAQ'
+};
+
+type PageProps = { params: Promise<{ section: string }> };
+
+export default async function Page(props: PageProps) {
+  const params = await props.params;
+  return (
+    <PageContainer scrollable>
+      <div className='flex-1 space-y-4'>
+        <Suspense fallback={<FormCardSkeleton />}>
+          <DeviceViewPage deviceId={params.deviceId} />
+        </Suspense>
+      </div>
+    </PageContainer>
+  );
+}
